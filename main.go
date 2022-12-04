7d71c6646dfd47b4dceae4051c44e11a06c03f15
RudyWang <a86742902@gmail.com>
2022-12-04 11:10:15 +0800

    delete unnecessary code

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"go-interview/api/apis/collections"
 	_ "go-interview/api/config"
 	"go-interview/api/database"
 	"go-interview/api/router"
@@ -9,7 +8,6 @@ import (
 
 func main() {
 	database.SelectCollection()
-	collections.UserText_r_all()
 	router := router.InitRouter()
 	router.Run(":5555")
 }
